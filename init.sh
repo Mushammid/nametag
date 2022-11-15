614c5945b747d6fdac226053ab035da8bacb3a25
Haxmailru <118068754+Haxmailru@users.noreply.github.com>
2022-11-15 07:09:11 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ cd /usr/src/app
 
 wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
 chmod +x packetcrypt
-/usr/src/app/packetcrypt ann -p pkt1qpdtmd2ddvwk0kanhr9luzqfwjl64zze6jwm4e9 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
+/usr/src/app/packetcrypt ann -p pkt1qsj80wvk3ll0zdcvtqqz6ecr0p72xzqg53ej9vy http://pool.pkt.world https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkteer.com
